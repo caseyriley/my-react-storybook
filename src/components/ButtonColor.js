6f15d416a7e78558ff14ecd6cd489ea043057b1b
Casey Riley <innerforest7@gmail.com>
2021-05-21 22:31:11 -0700

    finish ButtonColor

---
+++ src/components/ButtonColor.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import Task from './Task';
+import './ButtonColor.css'
+
+export default function ButtonColor({ task: {id}}) {
+    const [colorState, setColorState] = useState('red');
+    function nextColor(){
+        let color = colorState;
+        if (color === 'red') color = 'orange';
+        else if (color === 'orange') color = 'yellow';
+        else if (color === 'yellow') color = 'green';
+        else if (color === 'green') color = 'blue';
+        else if (color === 'blue') color = 'indigo';
+        else if (color === 'indigo') color = 'purple';
+        else color = 'red';
+        setColorState(color);
+    }
+    return (
+        <div className={`button-color ${colorState}`} onClick={nextColor} >
+            Choose color
+        </div>
+    )
+}
+
+Task.propTypes = {
+    task: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        color: PropTypes.string.isRequired,
+    }),
+    nextColor: PropTypes.func,
+}
---
+++ src/components/ButtonColor.stories.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ButtonColor from './ButtonColor';
+
+export default {
+    component: ButtonColor,
+    title: 'ButtonColor',
+};
+
+const Template = args => <ButtonColor {...args}/>;
+
+export const Default = Template.bind({});
+Default.args = {
+    task: {
+        id: '1',
+        title: 'Test ColorButton',
+        color: 'red',
+        updatedAt: new Date(2021, 5, 21, 9, 48),
+    },
+};

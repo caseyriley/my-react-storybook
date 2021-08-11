434e82fd9f35c57fbc84e27a2cefb40a67e1ebfc
caseyriley <innerforest7@gmail.com>
2021-08-10 22:23:29 -0700

    create Stretch.js and stretch.css

---
+++ src/components/Stretch.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Stretch = () => {
+  return (
+    <a href="#" class="button">
+      <canvas class="button__canvas"></canvas>
+      <span class="button__text">Hover me I am liquid</span>
+    </a>
+  );
+};

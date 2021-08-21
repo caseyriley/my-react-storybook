a4c3c2bcd72876856c1a626287401912547f944e
caseyriley <innerforest7@gmail.com>
2021-08-20 22:25:06 -0700

    update Stretch

--- src/components/ButtonColor.js
+++ src/components/ButtonColor.js
@@ -1,31 +1,31 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import Task from './Task';
-import './ButtonColor.css'
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import Task from "./Task";
+import "./ButtonColor.css";
 
-export default function ButtonColor({ task: {id}}) {
-    const [colorState, setColorState] = useState('red');
-    function nextColor(){
-        let color = colorState;
-        if (color === 'red') color = 'orange';
-        else if (color === 'orange') color = 'yellow';
-        else if (color === 'yellow') color = 'green';
-        else if (color === 'green') color = 'blue';
-        else if (color === 'blue') color = 'indigo';
-        else if (color === 'indigo') color = 'purple';
-        else color = 'red';
-        setColorState(color);
-    }
-    return (
-        <div className={`button-color ${colorState}`} onClick={nextColor} >
-            Choose color
-        </div>
-    )
+export default function ButtonColor({ task: { id } }) {
+  const [colorState, setColorState] = useState("red");
+  function nextColor() {
+    let color = colorState;
+    if (color === "red") color = "orange";
+    else if (color === "orange") color = "yellow";
+    else if (color === "yellow") color = "green";
+    else if (color === "green") color = "blue";
+    else if (color === "blue") color = "indigo";
+    else if (color === "indigo") color = "purple";
+    else color = "red";
+    setColorState(color);
+  }
+  return (
+    <div className={`button-color ${colorState}`} onClick={nextColor}>
+      Choose color
+    </div>
+  );
 }
 
 Task.propTypes = {
-    task: PropTypes.shape({
-        id: PropTypes.string.isRequired,
-    }),
-    nextColor: PropTypes.func,
-}
+  task: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+  }),
+  nextColor: PropTypes.func,
+};

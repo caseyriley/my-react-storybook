70e7ac900c5bb7b947850a40d6bc9f2f6746060f
caseyriley <innerforest7@gmail.com>
2021-08-25 21:48:30 -0700

    cleanup ExploreFilter

--- src/components/ExploreFilter.stories.js
+++ src/components/ExploreFilter.stories.js
@@ -7,81 +7,82 @@ export default {
   component: ExploreFilter,
 };
 
-const Template = (args) => (
-  <ExploreFilter {...args} />
-);
+const Template = (args) => <ExploreFilter {...args} />;
 
 export const Asia = Template.bind({});
 Asia.args = {
-  countryNames: ["Hong Kong", "India", "Indonesia", "Kenya", "Laos", "Malaysia", "Philippines", "United States Of America"],
+  countryNames: [
+    "Hong Kong",
+    "India",
+    "Indonesia",
+    "Kenya",
+    "Laos",
+    "Malaysia",
+    "Philippines",
+    "United States Of America",
+  ],
   country: {
-    "All": {
-      "BusinessTypes": [
+    All: {
+      BusinessTypes: [
         "eco-accommodations",
         "ethical experiences",
         "Resposible Travel Curators",
         "socially responsible eateries",
       ],
-      "Prices": ["under $200", "under $400", "under $600", "under $800", "under $1000", "over $1000"],
-      "SearchString": "SEARCH ECO BUSINESSES",
+      Prices: [
+        "under $200",
+        "under $400",
+        "under $600",
+        "under $800",
+        "under $1000",
+        "over $1000",
+      ],
+      SearchString: "SEARCH ECO BUSINESSES",
     },
     "Hong Kong": {
-      "BusinessTypes": ["eco-accommodations"],
-      "Prices": ["$", "$$"],
-      "SearchString": "SEARCH ECO BUSINESSES",
+      BusinessTypes: ["eco-accommodations"],
+      Prices: ["$", "$$"],
+      SearchString: "SEARCH ECO BUSINESSES",
     },
-    "India": {
-      "BusinessTypes": [
+    India: {
+      BusinessTypes: [
         "eco-accommodations",
         "ethical experiences",
         "Resposible Travel Curators",
         "socially responsible eateries",
       ],
-      "Prices": ["$", "$$"],
-      "SearchString": "SEARCH ECO BUSINESSES",
+      Prices: ["$", "$$"],
+      SearchString: "SEARCH ECO BUSINESSES",
     },
-    "Indonesia": {
-      "BusinessTypes": [
-        "eco-accommodations",
-      ],
-      "Prices": ["$", "$$"],
-      "SearchString": "SEARCH ECO BUSINESSES",
+    Indonesia: {
+      BusinessTypes: ["eco-accommodations"],
+      Prices: ["$", "$$"],
+      SearchString: "SEARCH ECO BUSINESSES",
     },
-    "Kenya": {
-      "BusinessTypes": [
-        "Responsible Travel Curators"
-      ],
-      "Prices": ["$"],
-      "SearchString": "SEARCH ECO BUSINESSES",
+    Kenya: {
+      BusinessTypes: ["Responsible Travel Curators"],
+      Prices: ["$"],
+      SearchString: "SEARCH ECO BUSINESSES",
     },
-    "Laos": {
-      "BusinessTypes": [
-        "eco-accommodations",
-      ],
-      "Prices": ["$$$"],
-      "SearchString": "SEARCH ECO BUSINESSES",
+    Laos: {
+      BusinessTypes: ["eco-accommodations"],
+      Prices: ["$$$"],
+      SearchString: "SEARCH ECO BUSINESSES",
     },
-    "Malaysia": {
-      "BusinessTypes": [
-        "eco-accommodations",
-      ],
-      "Prices": ["$$$"],
-      "SearchString": "SEARCH ECO BUSINESSES",
+    Malaysia: {
+      BusinessTypes: ["eco-accommodations"],
+      Prices: ["$$$"],
+      SearchString: "SEARCH ECO BUSINESSES",
     },
-    "Philippines": {
-      "BusinessTypes": [
-        "eco-accommodations",
-        "socially responsible eateries",
-      ],
-      "Prices": ["$$"],
-      "SearchString": "SEARCH ECO BUSINESSES",
+    Philippines: {
+      BusinessTypes: ["eco-accommodations", "socially responsible eateries"],
+      Prices: ["$$"],
+      SearchString: "SEARCH ECO BUSINESSES",
     },
     "United States Of America": {
-      "BusinessTypes": [
-        "eco-accommodations",
-      ],
-      "Prices": ["$$"],
-      "SearchString": "SEARCH ECO BUSINESSES",
+      BusinessTypes: ["eco-accommodations"],
+      Prices: ["$$"],
+      SearchString: "SEARCH ECO BUSINESSES",
     },
   },
 };
--- src/components/ExploreFilter.stories.tsx
+++ src/components/ExploreFilter.stories.tsx
@@ -13,75 +13,71 @@ const Template: Story<ExploreFilterProps> = (args) => (
 
 export const Asia = Template.bind({});
 Asia.args = {
-  countryNames: ["Hong Kong", "India", "Indonesia", "Kenya", "Laos", "Malaysia", "Philippines", "United States Of America"],
+  countryNames: [
+    "Hong Kong",
+    "India",
+    "Indonesia",
+    "Kenya",
+    "Laos",
+    "Malaysia",
+    "Philippines",
+    "United States Of America",
+  ],
   country: {
-    "All": {
-      "BusinessTypes": [
+    All: {
+      BusinessTypes: [
         "eco-accommodations",
         "ethical experiences",
         "Resposible Travel Curators",
         "socially responsible eateries",
       ],
-      "Prices": ["$", "$$"],
-      "SearchString": "SEARCH ASIAN BUSINESSES",
+      Prices: ["$", "$$"],
+      SearchString: "SEARCH ASIAN BUSINESSES",
     },
     "Hong Kong": {
-      "BusinessTypes": ["eco-accommodations"],
-      "Prices": ["$", "$$"],
-      "SearchString": "SEARCH ASIAN BUSINESSES",
+      BusinessTypes: ["eco-accommodations"],
+      Prices: ["$", "$$"],
+      SearchString: "SEARCH ASIAN BUSINESSES",
     },
-    "India": {
-      "BusinessTypes": [
+    India: {
+      BusinessTypes: [
         "eco-accommodations",
         "ethical experiences",
         "Resposible Travel Curators",
         "socially responsible eateries",
       ],
-      "Prices": ["$", "$$"],
-      "SearchString": "SEARCH ASIAN BUSINESSES",
+      Prices: ["$", "$$"],
+      SearchString: "SEARCH ASIAN BUSINESSES",
     },
-    "Indonesia": {
-      "BusinessTypes": [
-        "eco-accommodations",
-      ],
-      "Prices": ["$", "$$"],
-      "SearchString": "SEARCH ASIAN BUSINESSES",
+    Indonesia: {
+      BusinessTypes: ["eco-accommodations"],
+      Prices: ["$", "$$"],
+      SearchString: "SEARCH ASIAN BUSINESSES",
     },
-    "Kenya": {
-      "BusinessTypes": [
-        "Responsible Travel Curators"
-      ],
-      "Prices": ["$"],
-      "SearchString": "SEARCH ASIAN BUSINESSES",
+    Kenya: {
+      BusinessTypes: ["Responsible Travel Curators"],
+      Prices: ["$"],
+      SearchString: "SEARCH ASIAN BUSINESSES",
     },
-    "Laos": {
-      "BusinessTypes": [
-        "eco-accommodations",
-      ],
-      "Prices": ["$$$"],
-      "SearchString": "SEARCH ASIAN BUSINESSES",
+    Laos: {
+      BusinessTypes: ["eco-accommodations"],
+      Prices: ["$$$"],
+      SearchString: "SEARCH ASIAN BUSINESSES",
     },
-    "Malaysia": {
-      "BusinessTypes": [
-        "eco-accommodations",
-      ],
-      "Prices": ["$$$"],
-      "SearchString": "SEARCH ASIAN BUSINESSES",
+    Malaysia: {
+      BusinessTypes: ["eco-accommodations"],
+      Prices: ["$$$"],
+      SearchString: "SEARCH ASIAN BUSINESSES",
     },
-    "Philippines": {
-      "BusinessTypes": [
-        "eco-accommodations",
-        "socially responsible eateries",
-      ],
-      "Prices": ["$$"],
-      "SearchString": "SEARCH ASIAN BUSINESSES",
+    Philippines: {
+      BusinessTypes: ["eco-accommodations", "socially responsible eateries"],
+      Prices: ["$$"],
+      SearchString: "SEARCH ASIAN BUSINESSES",
     },
     "United States Of America": {
-      "BusinessTypes": [
-        "eco-accommodations",
-      ],
-      "Prices": ["$$"],
-      "SearchString": "SEARCH ASIAN BUSINESSES",
+      BusinessTypes: ["eco-accommodations"],
+      Prices: ["$$"],
+      SearchString: "SEARCH ASIAN BUSINESSES",
     },
   },
 };
